0b23d8ca0627f16c6912acb9f8e9a833cddf8929
Michal Soldan <michal.soldan@actumdigital.com>
2025-04-28 05:48:58 -0400

    formatting

--- src/XperienceCommunity.Bynder/Admin/Client/src/form-components/Asset.ts
+++ src/XperienceCommunity.Bynder/Admin/Client/src/form-components/Asset.ts
@@ -1,13 +1,13 @@
 ﻿import { File, assetType } from "@bynder/compact-view";
 
 export interface Asset {
-  readonly id: string;
-  readonly name: string;
-  readonly description: string;
-  readonly url: string;
-  readonly extensions: string[];
-  readonly tags: string[];
-  readonly databaseId: string;
-  readonly type: assetType;
-  readonly files: Record<string, File>;
+    readonly id: string;
+    readonly name: string;
+    readonly description: string;
+    readonly url: string;
+    readonly extensions: string[];
+    readonly tags: string[];
+    readonly databaseId: string;
+    readonly type: assetType;
+    readonly files: Record<string, File>;
 }
--- src/XperienceCommunity.Bynder/Admin/Client/src/form-components/BynderSelectorClientProps.ts
+++ src/XperienceCommunity.Bynder/Admin/Client/src/form-components/BynderSelectorClientProps.ts
@@ -2,7 +2,7 @@ import { FormComponentProps } from "@kentico/xperience-admin-base";
 import { assetType } from "@bynder/compact-view";
 
 export interface BynderSelectorClientProps extends FormComponentProps {
-  readonly minimumAssets: number;
-  readonly maximumAssets: number;
-  readonly allowedTypes: assetType[];
+    readonly minimumAssets: number;
+    readonly maximumAssets: number;
+    readonly allowedTypes: assetType[];
 }
